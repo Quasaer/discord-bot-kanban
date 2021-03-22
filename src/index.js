ecd1cfac9d034a2270201803ebd8430865bb97cd
MuminRahman2001 <65430144+MuminRahman2001@users.noreply.github.com>
2021-03-22 22:19:18 +0000

    Testing
    Fixing codes and errors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 require('dotenv').config();
-const client = new Discord.Client();
+
+const { Client } = require('discord.js');
+
+const client = new Client();
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
   if (msg.content === 'ping') {
-    msg.reply('Pong!');
+    msg.reply('HAii!');
   }
 });
+
+client.on('message', msg => {
+    if (msg.content === 'sting') {
+      msg.reply('HAii!');
+    }
+  });
 
 client.login(process.env.LOGIN_TOKEN);
