02055033cdf41746bf30b93611730db069a16711
lewisj2501 <jones.lewis2501@gmail.com>
2021-03-22 16:39:34 +0000

    Update index.js
    Testing for committed changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js'); // this for the 
 const client = new Discord.Client();
 require('dotenv').config();
 client.on('ready', () => {
