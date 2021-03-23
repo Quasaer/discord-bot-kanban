1fbe85f24e0fbeaf78ee684bc97b318cb0f1efed
MuminRahman2001 <65430144+MuminRahman2001@users.noreply.github.com>
2021-03-23 03:24:46 +0000

    New bot file
    Completely revamped my version of the bot file to be more accessible and user friendly and can now fully be used as starter code.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,44 @@
-require('dotenv').config();
-
+const config = require('./config.json');
+const command = require('./cmd/command.js')
 const { Client } = require('discord.js');
-
 const client = new Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
+
+    command(client, ['ping', 'test'], (message) => {
+        message.channel.send('Pong!');
+    });
+
+    command(client, 'help', (message) => {
+        message.channel.send(`
+These are the supported commands for the KAnban Bot.
+The default prefix for the Kanban Bot is %.
+
+**%status <INPUT>** - Updates bot status 
+**%ping** - Returns 'Pong!'
+        `); //not indented since it'll indent on discord as well
+    });
+
+    const { prefix } = config;
+    client.user.setPresence({
+        activity: {
+            name:  `${prefix}help for help` //using prefix as template literal for future use
+        }
+    });
+
+    command(client, 'status', message => { //update status of bot
+        const content = message.content.replace('%status ', ''); // "%print hello" => "hello"
+
+        client.user.setPresence({
+            activity: {
+                name: content,
+                type: 0
+            },
+        });
+    });
+
+    
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('HAii!');
-  }
-});
-
-client.on('message', msg => {
-    if (msg.content === 'sting') {
-      msg.reply('Hey');
-    }
-  });
-
-client.login(process.env.LOGIN_TOKEN);
+client.login(config.token);
