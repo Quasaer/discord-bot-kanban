65a450f428ed4fe6afbe5faba82426ca554c7db4
lewisj2501 <jones.lewis2501@gmail.com>
2021-03-25 15:38:15 +0000

    Update index.js
    Added comments to code to better understand the index.js. Watched the meeting recording to write the notes.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-const fs = require('fs');
-const Discord = require('discord.js');
-require('dotenv').config();
-const path = require('path');
+const fs = require('fs'); 
+const Discord = require('discord.js'); 
+require('dotenv').config(); 
+const path = require('path'); 
+// line1: gathers all commands as a collection and it also executes the events
+// line2: directory module built into node.js
+// line3: get discord.js
+// line4: gets data from .env file
+// line5: paths the relative paths
+
 
 const client = new Discord.Client();
-client.commands = new Discord.Collection;
-
+client.commands = new Discord.Collection; // compliation of export modules from the commands files
+// reads the commands and events directory and scans through each file
 const commandFiles = fs.readdirSync(path.resolve(__dirname, './commands')).filter(file=>file.endsWith('.js'));
 const eventFiles = fs.readdirSync(path.resolve(__dirname, './events')).filter(file=>file.endsWith('.js'));
 
-
+// gets each file and puts it the collection
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 
@@ -17,7 +23,7 @@ for (const file of commandFiles) {
 	// with the key as the command name and the value as the exported module
 	client.commands.set(command.name, command);
 }
-
+// gets each event and executes them
 for (const file of eventFiles) {
 	const event = require(`./events/${file}`);
 	if (event.once) {
@@ -26,5 +32,5 @@ for (const file of eventFiles) {
 		client.on(event.name, (...args) => event.execute(...args, client));
 	}
 }
-
+// login token for the bot
 client.login(process.env.LOGIN_TOKEN); 
