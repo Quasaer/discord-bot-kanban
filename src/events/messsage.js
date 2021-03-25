f8d6799e9b660abfd818ca5e8060e69500074b75
lewisj2501 <jones.lewis2501@gmail.com>
2021-03-25 17:13:43 +0000

    Update messsage.js
    Atm, I have just hard coded the channel id in the .env so that it now checks before a command is executed that it is on the right channel. I would like to implement this feature using a bind command but without a database to get the channel id from and without the ability to set a global flag in the .env file to state whether a channel bind is true or false, I don't believe I can go further with implementing this.

--- src/events/messsage.js
+++ src/events/messsage.js
@@ -6,6 +6,7 @@ module.exports = {
 		const args = message.content.slice(prefix.length).trim().split(/ +/); // regex for cutting space
 		const command = args.shift().toLowerCase(); // turns all text to lowercase to avoid case issues
 		if (!client.commands.has(command)) return; // finds if the command exists, if not do nothing
+		if (message.channel.id != process.env.CHANNEL_ID) return;
 		try {
 			client.commands.get(command).execute(message, args); // try to execute it
 		} catch (error) { // if not return error msg
