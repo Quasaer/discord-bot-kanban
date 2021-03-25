9230bcecba7ca55809d5102a8bac8493de4f4252
lewisj2501 <jones.lewis2501@gmail.com>
2021-03-25 15:45:39 +0000

    Update messsage.js
    Added comments to better understand what the file and its functions actually do (took notes from meeting recording).

--- src/events/messsage.js
+++ src/events/messsage.js
@@ -2,15 +2,15 @@ module.exports = {
 	name: 'message',
 	execute(message, client) {
 		const prefix = '%';
-		if (!message.content.startsWith(prefix) || message.author.bot) return;
-		const args = message.content.slice(prefix.length).trim().split(/ +/);
-		const command = args.shift().toLowerCase();
-		if (!client.commands.has(command)) return;
+		if (!message.content.startsWith(prefix) || message.author.bot) return; // finds prefix, and trims it
+		const args = message.content.slice(prefix.length).trim().split(/ +/); // regex for cutting space
+		const command = args.shift().toLowerCase(); // turns all text to lowercase to avoid case issues
+		if (!client.commands.has(command)) return; // finds if the command exists, if not do nothing
 		try {
-			client.commands.get(command).execute(message, args);
-		} catch (error) {
-			console.log(error);
-			message.reply('There was an error trying to execute that command');
+			client.commands.get(command).execute(message, args); // try to execute it
+		} catch (error) { // if not return error msg
+			console.log(error); // console error
+			message.reply('There was an error trying to execute that command'); // discord channel error
 		}
 	},
 };
