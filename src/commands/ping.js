48dcf0a2930f6988d201259673c0bf397050a764
lewisj2501 <jones.lewis2501@gmail.com>
2021-03-25 15:50:54 +0000

    Update ping.js
    Made a comment on what module.exports does (took note from meeting recording).

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,4 +1,4 @@
-module.exports = {
+module.exports = { // makes code visible to node.js and discord.js, use this on all the commands otherwise it won't work
 	name: 'ping',
 	description: 'Ping!',
 	execute(message, args) {
