e578ccd4b4c0dd5ed2f33de9a08d9feec14c9043
lewisj2501 <jones.lewis2501@gmail.com>
2021-03-26 14:00:18 +0000

    Update messsage.js
    Added the logic from the bind command. If a bind is not set or if the channel id equals the channel id set, then run commands. Else, if there has been a channel bind set then it won't allow users to message the bot.

--- src/events/messsage.js
+++ src/events/messsage.js
@@ -6,12 +6,18 @@ module.exports = {
 		const args = message.content.slice(prefix.length).trim().split(/ +/); // regex for cutting space
 		const command = args.shift().toLowerCase(); // turns all text to lowercase to avoid case issues
 		if (!client.commands.has(command)) return; // finds if the command exists, if not do nothing
-		if (message.channel.id != process.env.CHANNEL_ID) return;
-		try {
-			client.commands.get(command).execute(message, args); // try to execute it
-		} catch (error) { // if not return error msg
-			console.log(error); // console error
-			message.reply('There was an error trying to execute that command'); // discord channel error
+		//if (process.env.CHANNEL_BIND_ID == FALSE)
+		//if (message.channel.id != process.env.CHANNEL_BIND_ID) return;
+		if (process.env.CHANNEL_BIND_ID == FALSE || message.channel.id === process.env.CHANNEL_BIND_ID){
+			try {
+				client.commands.get(command).execute(message, args); // try to execute it
+			} catch (error) { // if not return error msg
+				console.log(error); // console error
+				message.reply('There was an error trying to execute that command'); // discord channel error
+			}
+		}
+		else{
+			return;
 		}
 	},
 };
