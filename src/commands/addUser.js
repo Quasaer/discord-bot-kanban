2b02a4eda14584b637451bb07e7e8e1c901d107d
MuminRahman2001 <65430144+MuminRahman2001@users.noreply.github.com>
2021-03-27 20:22:04 +0000

    funcitonal adduser and find user functions
    adduser command fully functional

--- src/commands/addUser.js
+++ src/commands/addUser.js
@@ -4,13 +4,17 @@ module.exports = {
 	description: 'add <variable> <name>',
 	execute(message, args) {
 		const target = message.mentions.users.first() || message.author;
-		const resp  = dbCmd.addUser(target.tag);
-		// console.log(resp);
-		if(resp){
-			const user = target.tag.split('#');
-			message.channel.send(`${user[0]} has successfully been added to DB`);
-		} else {
-			console.log('error saving user to database')
+		const find = dbCmd.findUser(target.tag);
+		const user = target.tag.split('#');
+		if(find){
+			message.channel.send(`${user[0]} already exists in the DB`);
+		} else{
+			const resp  = dbCmd.addUser(target.tag);
+			if(resp){
+				message.channel.send(`${user[0]} has successfully been added to DB`);
+			} else {
+				console.log('error saving user to database')
+			}
 		}
 	},
 };
--- src/dbCommands.js
+++ src/dbCommands.js
@@ -16,11 +16,18 @@ const Columns = require('./models/Columns')(sequelize, Sequelize.DataTypes);
 const Config = require('./models/Config')(sequelize, Sequelize.DataTypes);
 const Task_Assignment = require('./models/Task_Assignment')(sequelize, Sequelize.DataTypes);
 
-async function addUser(username) {
-		const created_at = Math.floor(+new Date() / 1000); 
-		await Users.create({ discord_username: username, created_at_date_stamp: created_at}).catch(error => {
-			console.log(error);
-		});
+async function addUser(username) { //function to add user
+	const created_at = Math.floor(+new Date() / 1000); //calculates date as integer
+	await Users.create({ discord_username: username, created_at_date_stamp: created_at}).catch(error => { //adds to database
+		console.log(error);
+	});
 };
 
-module.exports = { Users, addUser }; //only export function calls
+async function findUser(username) { //function to find user
+	const finduser = await Users.findOne({
+		where: { discord_username: username }, //attempts to match username paramter (target.tag) to column name discord_username
+	});
+	return finduser;
+};
+
+module.exports = { Users, addUser, findUser }; //only export function calls
