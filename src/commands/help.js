a4e518ca5c3e19865eb73c42dece6e43311af10b
WDowns2 <77688462+WDowns2@users.noreply.github.com>
2021-03-30 14:47:03 +0100

    Create help command

---
+++ src/commands/help.js
@@ -0,0 +1,49 @@
+const { MessageEmbed } = require("discord.js");
+const { readdirSync } = require("fs");
+const prefix = require("../../config.json").prefix;
+
+module.exports = {
+  name: "help",
+  aliases : ['h'],
+  description: "Shows all available bot commands.",
+  run: async (client, message, args) => {
+
+
+    const roleColor =
+      message.guild.me.displayHexColor === "#000000"
+        ? "#ffffff"
+        : message.guild.me.displayHexColor;
+
+    if (!args[0]) {
+      let categories = [];
+
+      readdirSync("./commands/").forEach((dir) => {
+        const commands = readdirSync(`./commands/${dir}/`).filter((file) =>
+          file.endsWith(".js")
+        );
+
+        const cmds = commands.map((command) => {
+          let file = require(`../../commands/${dir}/${command}`);
+
+          if (!file.name) return "No command name.";
+
+          let name = file.name.replace(".js", "");
+
+          return `\`${name}\``;
+        });
+
+        let data = new Object();
+
+        data = {
+          name: dir.toUpperCase(),
+          value: cmds.length === 0 ? "In progress." : cmds.join(" "),
+        };
+
+        categories.push(data);
+      });
+    } 
+  } 
+
+  
+
+   
