b83f1c45e0487c0c1f3ded6905b9d0e48e2b526a
lewisj2501 <jones.lewis2501@gmail.com>
2021-03-30 15:38:08 +0100

    Changed Skeleton Code
    Added a new bind command and used skeleton code from the adduser command to make a new one. Also, dbcommands was updated with two new functions to set and get the channel bind id.

--- src/commands/bind.js
+++ src/commands/bind.js
@@ -1,19 +1,21 @@
-require('dotenv').config();
-module.exports = { // makes code visible to node.js and discord.js, use this on all the commands otherwise it won't work
+let dbCmd  = require('../dbCommands.js');
+module.exports = {
 	name: 'bind',
 	description: 'Bind!',
 	execute(message, args) {
-        if (process.env.CHANNEL_BIND === FALSE){
-            process.env.CHANNEL_BIND_ID = message.channel.id;
-            process.env.CHANNEL_BIND = TRUE;
-            message.channel.send('Channel Binded!');
-        }
-        else if (process.env.CHANNEL_BIND === TRUE){
-            message.channel.send('Channel Already Binded!');
-        }
-        else{
-            message.channel.send('Can only bind to one specfic channel.');
-        }
-		
+		const target = message.channel.id;
+		const find = dbCmd.findBindID(target.tag).then((val) =>{
+			console.log(find);
+			if(val !== null){
+				message.channel.send(`The channel is already binded!`);
+			} else {
+				const resp  = dbCmd.addBindID(target.tag);
+				if(resp){
+					message.channel.send(`Channel has successfully been binded!`);
+				} else {
+					console.log('error saving user to database')
+				}
+			}
+		});
 	},
 };
--- src/dbCommands.js
+++ src/dbCommands.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize('database', 'username', 'password', {
 	logging: false,
 	storage: './src/KanbanDB.db',
 });
-
+// models
 const Users = require('./models/Users')(sequelize, Sequelize.DataTypes);
 const Boards = require('./models/Board')(sequelize, Sequelize.DataTypes);
 const Tasks = require('./models/Tasks')(sequelize, Sequelize.DataTypes);
@@ -15,13 +15,13 @@ const Column_Track = require('./models/Column_Track')(sequelize, Sequelize.DataT
 const Columns = require('./models/Column')(sequelize, Sequelize.DataTypes);
 const Config = require('./models/Config')(sequelize, Sequelize.DataTypes);
 const Task_Assignment = require('./models/Task_Assignment')(sequelize, Sequelize.DataTypes);
-
+// global scope
 async function addUser(username) { //function to add user
 	const created_at = Math.floor(+new Date() / 1000); //calculates date as integer
 	await Users.create({ discord_username: username, created_at_date_stamp: created_at}).catch(error => { //adds to database
 		console.log(error);
 	});
-};
+}; 
 
 async function findUser(username) { //function to find user
 	const finduser = await Users.findOne({
@@ -30,4 +30,17 @@ async function findUser(username) { //function to find user
 	return finduser;
 };
 
-module.exports = { addUser, findUser }; //only export function calls
+async function addBindID(channel_ID) { //function to add BindID to db
+	await Config.create({ channel_bind_id: channel_ID}).catch(error => { //adds to config table in database
+		console.log(error);
+	});
+}; 
+
+async function findBindID(channel_ID) { //function to find user
+	const findbind = await Config.findOne({
+		where: { channel_bind_id: channel_ID }, //attempts to match username paramter (target.tag) to column name discord_username
+	});
+	return findbind;
+};
+
+module.exports = { addUser, findUser, addBindID, findBindID }; //only export function calls
