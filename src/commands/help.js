e09c7764ac5343f788cb7e943d9f217354cbc539
WDowns2 <77688462+WDowns2@users.noreply.github.com>
2021-04-01 13:09:24 +0100

    Update help.js
    bold tiles of command names

--- src/commands/help.js
+++ src/commands/help.js
@@ -4,9 +4,9 @@ description:'Help!',
 execute(message, args) {
 message.channel.send(`
 These are my supported commands:
-**%help - Displays the help menu
-**%create-task- creates new task in column
-**%create-column-creates new column
+**%help** - Displays the help menu
+**%create-task**- creates new task in column
+**%create-column**-creates new column
 `);
 
 
