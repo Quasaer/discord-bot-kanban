528c380db2304823fc516a346220049b288727c5
WDowns2 <77688462+WDowns2@users.noreply.github.com>
2021-04-01 12:53:51 +0100

    Update help.js

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,10 +1,10 @@
 module.export={
 name: 'help',
 description:'Help!'
-execute(message){
+execute(message, args) {
 message.channel.send(`
 These are my supported commands:
-**%help** - Displays the help menu
+**%help - Displays the help menu
 **%create-task- creates new task in column
 **%create-column-creates new column
 `)
