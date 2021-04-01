d7ce9cc83eb44438679e820d00cc0fb09bdbb914
WDowns2 <77688462+WDowns2@users.noreply.github.com>
2021-04-01 12:47:11 +0100

    Update help.js
    Problems with previous code, new code implemented

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,21 +1,17 @@
-const config = require('./config.json')
-const command = require('./commands')
-
-
-  command(client, 'help', (message) => {
-    message.channel.send(`
+module.export={
+name: 'help',
+description:'Help!'
+execute(message,args){
+message.channel.send(`
 These are my supported commands:
 **%help** - Displays the help menu
 **%create-task- creates new task in column
 **%create-column-creates new column
 `)
-  })
 
-  const { prefix } = config
 
-  client.user.setPresence({
-    activity: {
-      name: `"${prefix}help" for help`,
-    },
-  })
+},
 
+
+
+};
