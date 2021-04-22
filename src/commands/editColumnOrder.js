f37a03c97f6dedd09f27e1f7214e97c66a80468a
MuminRahman2001 <65430144+MuminRahman2001@users.noreply.github.com>
2021-04-22 01:56:06 +0100

    Update editColumnOrder.js
    user can now input column order numbers but validation still work in progress

--- src/commands/editColumnOrder.js
+++ src/commands/editColumnOrder.js
@@ -1,40 +1,20 @@
 let dbCmd  = require('../dbCommands.js');
 let data = {};
+const embed = {
+	color: 0x0099ff,
+	title: '',
+	description: 'Column names and order numbers.',
+	thumbnail: {
+		url: 'https://i.dlpng.com/static/png/6905682_preview.png',
+	},
+	fields: [],
+};
 
-function editTask(message){
-    message.reply(`Here are your columns.\n`
-                + 'type `name` to change the name of the task.\n'
-                + 'type `description` to change the description of the task.\n'
-                + 'type `deadline` to change the deadline date of the task.\n'
-                + 'You have 30 seconds or else task will not be made.\n');
-
-    // message.channel.awaitMessages(m => m.author.id == message.author.id,
-    // {max: 1, time: 30000}).then(collected => {
-    //     if (collected.first().content.toLowerCase() === 'name') {
-    //         editName(message);
-    //     } else if(collected.first().content.toLowerCase() === 'description') {
-    //         editDescription(message);
-    //     } else if(collected.first().content.toLowerCase() === 'deadline') {
-    //         editDeadlineDate(message);
-    //     } else {
-    //         message.reply('That is not a valid response\n'
-    //         + 'Please retype edittask command');
-    //     }     
-    // }).catch(() => {
-	// 	message.reply('No answer after 30 seconds, operation canceled.');
-    // });
-}
-
-
-//final confirmation
-function finalConfirmation(message){
-	message.channel.send('re-order your columns:\n'
+function editColumnOrder(message){
+    message.reply('re-order your columns:\n'
 			+ '`example: 1, 3, 2, 4, ...`\n'
 			+ 'Your columns:\n'
 			+ 'You have 30 seconds or else task will not be made.\n');
-            for (let i = 0; i < data.columnCount; i++) {
-                message.channel.send('`'+`name:${data.columns[i].name}`+'`,`'+` ordernumber:${data.columns[i].column_order_number}`+'`')
-            }
             /*
 
             NEED TO CHECK IF THERE IS A BETTER WAY TO SHOW COLUMNS
@@ -42,6 +22,48 @@ function finalConfirmation(message){
 
             */
 
+
+    message.channel.awaitMessages(m => m.author.id == message.author.id,
+    {max: 1, time: 30000}).then(collected => {
+        let columnOrderList = collected.first().content.toLowerCase().split(/[ ,]+/);
+		columnCountValidation = data.columnCount - 1;
+		if(columnOrderList.length !== data.columnCount){
+			message.reply(`You have not inputted enough columns order numbers, there should be ${data.columnCount} not ${columnOrderList.length}!`);
+			editColumnOrder(message);
+		// } else if(columnOrderList.length !== data.columnCount){
+		// 	message.reply(`You have not inputted enough columns order numbers, there should be ${data.columnCount} not ${columnOrderList.length}!`);
+		// 	editColumnOrder(message);
+
+		} else {
+			let ColumnOrderInputArray = [];
+			for (let i = 0; i < columnOrderList.length; i++) {
+				let number = parseInt(columnOrderList[i]);
+				let numberValidation = Number.isInteger(columnOrderList);
+				// if(Number.isInteger(123) === true){
+				// 	console.log(numberValidation);
+				// 	ColumnOrderInputArray.push(columnOrderList[i]);
+				// } else{
+				// 	message.reply('You can only input integers for the column order apart from `,');
+				// 	editColumnOrder(message);
+				// };
+				/*
+					check if inputs are all numbers
+					check if they start from 1 to the column count
+				*/
+				console.log(numberValidation);
+				console.log(number);
+			}
+			
+			}
+    }).catch(() => {
+		message.reply('No answer after 30 seconds, operation canceled.');
+    });
+}
+
+
+//final confirmation
+function finalConfirmation(message){
+	
 	message.channel.awaitMessages(m => m.author.id == message.author.id,
 	{max: 1, time: 30000}).then(collected => {
 		if (collected.first().content.toLowerCase() === 'yes') {
@@ -69,6 +91,7 @@ function updateDatabase(message){
 function setData() {
 	data = {
 		userid:'',
+		board:{},
 		columns:{},
         columnCount: 0,
 	};
@@ -90,6 +113,7 @@ module.exports = {
 				data.userid = userModel.user_id;
 			});
 			dbCmd.findBoardByName(boardNameInput).then((boardModel) =>{
+				data.board['name']=boardModel.name;
 				if(boardModel !== null){
                     dbCmd.findAllColumnNamesByBoardId(boardModel.board_id).then((columnModels) =>{
 						if(columnModels.length !== 0){
@@ -103,7 +127,20 @@ module.exports = {
                                 };
                                 data.columnCount ++;
                             };
-                            finalConfirmation(message)
+							embed.title = data.board['name'];
+							for (let i = 0; i < data.columnCount; i++) {
+
+								let field = {};
+
+								field['name'] = data.columns[i].name;
+								field['value'] = `${data.columns[i].column_order_number}`;
+
+								field['inline'] = true;
+								embed['fields'].push(field);
+							}
+
+							message.channel.send({ embed: embed });
+                            editColumnOrder(message)
                             //
 						} else {
 							message.channel.send(`The board ${boardNameInput} does not have any existing columns`);
